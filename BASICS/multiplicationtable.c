ffe749d0492ca12bafab44e192844f43ebd17cd3
Asish Mehata <mehataasish@gmail.com>
2024-09-06 18:18:30 +0545

    BASICS

---
+++ BASICS/multiplicationtable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n, table = 1;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    // for (int i =1; i<=10; i++)
+    // {
+    //     table = n * i;
+    //     printf("%d\n ", table);
+    // }
+
+    for (int i = 1; i <=10; i++)
+    {
+        printf("%d \n", n*i);
+    }
+    
+    return 0;
+}
